a7d294bdf8cc49fff56be94ff72539a8e3b913b9
Nicolas <nicolasgfalabella@gmail.com>
2024-01-30 10:22:54 -0300

    Consultas sql

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,9 +1,34 @@
 const { NextResponse } = require("next/server");
+import { conn } from "@/libs/mysql";
 
 export function GET(){
     return NextResponse.json({ message : "Listando Productos"})
 }
 
-export function POST() {
-    return NextResponse.json('Creando Producto')
+export async function POST( request ) {
+    try {
+        const {name ,descripcion , price } = await request.json()   
+        const result = await conn.query("INSERT INTO product SET ?" , {
+            name,
+            descripcion,
+            price
+        })
+        console.log(result)
+        return NextResponse.json({
+            id : result.insertId,
+            name,
+            descripcion,
+            price
+        })
+    } catch (error) {
+        console.log( error )
+        return NextResponse.json(
+            { 
+                message:error.message 
+            } ,
+            {
+                status : 500
+            }
+       )
+    } 
 }
