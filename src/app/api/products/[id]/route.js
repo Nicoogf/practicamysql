6fca6ec4b3e769500ef7f791396dd37316b1a141
Nicolas <nicolasgfalabella@gmail.com>
2024-01-31 08:51:48 -0300

    Consulta de producto por id

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,7 +1,15 @@
 const { NextResponse } = require("next/server");
+import { conn  } from "@/libs/mysql";
 
-export function GET() {
-    return NextResponse.json('Obteniendo Producto')}
+export async function GET( request , { params }) {
+   try {
+   const result = await conn.query("SELECT * FROM product WHERE ID = ?" , [params.id])
+   if( result.length === 0) return NextResponse.json({message : "Producto no encontrado"})
+   return NextResponse.json( result[0] )
+   } catch (error) {
+    return NextResponse.json({message : error.message} , {status : 500})
+   }
+}
 
 export function DELETE(){
     return NextResponse.json('Eliminando Producto')
--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,8 +1,18 @@
 const { NextResponse } = require("next/server");
 import { conn } from "@/libs/mysql";
 
-export function GET(){
-    return NextResponse.json({ message : "Listando Productos"})
+export async function GET(){
+  try {
+    const results = await conn.query( "SELECT * FROM product")
+    return NextResponse.json(results)
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({message : error.message},
+        {
+            status : 500
+        }
+    )
+  }
 }
 
 export async function POST( request ) {
